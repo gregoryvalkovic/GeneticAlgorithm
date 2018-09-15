4e0c36343a61835cada43c1cc67d813ba184cbbd
Gregory Valkovic <gregoryvalkovic@gmail.com>
2018-09-15 19:39:55 +1000

    Read input file
    +Build invector table from input file
    +Print table contents

--- drive.c
+++ drive.c
@@ -9,6 +9,10 @@
 #include "pop.h"
 #include "gene.h"
 
+/* Function prototypes */
+void printTable(InVTable invt);
+void buildTable(InVTable *invt, char *fileName);
+
 void test_pcbmill(void){
 	/* TO DO */
 
@@ -62,12 +66,57 @@ void test_minfn(void){
 }
 
 int main(int argc, char *argv[]){
+	char *fileName = argv[inputFile];
+	InVTable invt;
 
-	/* TO DO */
 
 	/* The only point at which srand should be called */
 	srand(SRAND_SEED);
 
+	invector_init(&invt);
+	buildTable(&invt, fileName);
+	printTable(invt);
+
 	return EXIT_SUCCESS;
 }
 
+
+void buildTable(InVTable *invt, char *fileName) {
+	FILE *fp;
+	char line[INV_LEN];
+	char *allele;
+	int i;
+
+	fp = fopen(fileName, "r");
+
+	/* Read file line by line*/
+	while (fgets(line, INV_LEN, fp) != NULL) {
+		allele = strtok(line, INV_DELIM2);
+		allele = strtok(NULL, INV_DELIM2);
+		allele = strtok(allele, INV_DELIM3);
+
+		/* Build table */
+		for (i=0; i < invt->width; i++) {
+			invt->table[invt->tot][i] = atoi(allele);
+			allele = strtok(NULL, INV_DELIM3);
+		}
+		invt->tot++;
+	}
+	fclose(fp);
+}
+
+
+void printTable(InVTable invt) {
+	int i, j;
+
+	/* Loop through all rows */
+	for (i=0; i < invt.tot; i++) {
+		printf("%d: ", i);
+
+		/* Loop through each allele */
+		for (j=0; j < invt.width; j++) {
+			printf("%d ", invt.table[i][j]);
+		}
+		printf("\n");
+	}
+}
